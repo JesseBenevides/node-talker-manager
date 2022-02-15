ceb25278578896215eae7e510cc328eb47658040
Jesse Benevides <jessebenevides@gmail.com>
2022-02-15 16:29:47 -0300

    [REQ 1] Cria rota get /talker que retorna todos talkers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorHandler = require('./middlewares/errorHandler');
+const getTalkers = require('./middlewares/getTalkers');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +13,10 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', getTalkers);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log('Online');
