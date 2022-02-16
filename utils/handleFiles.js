af03d16ff77d0765bb6fd0f2622e88fd4d5b7f0c
Jesse Benevides <jessebenevides@gmail.com>
2022-02-16 17:05:01 -0300

    [UTILS] Adiciona função pra reescrever a lista de palestrantes no arquivo

--- utils/handleFiles.js
+++ utils/handleFiles.js
@@ -1,7 +1,14 @@
 const fs = require('fs/promises');
 
-const filePath = ('./talker.json');
+const FILEPATH = ('./talker.json');
 
-const readTalkers = async () => JSON.parse(await fs.readFile(filePath));
+const readTalkers = async () => JSON.parse(await fs.readFile(FILEPATH));
 
-module.exports = { readTalkers };
+const writeTalkers = async (talkerList) => {
+  // const talkerList = await readTalkers();
+  // talkerList.push(newTalker);
+  await fs.writeFile(FILEPATH, JSON.stringify(talkerList));
+  return talkerList;
+};
+
+module.exports = { readTalkers, writeTalkers };
