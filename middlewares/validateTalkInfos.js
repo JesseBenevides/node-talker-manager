146f74ff73787411f320ad3224e3425704425ad3
Jesse Benevides <jessebenevides@gmail.com>
2022-02-16 19:20:52 -0300

    [REQ 5] Ajusta verifição na validação do talk.rate"

--- middlewares/validateTalkInfos.js
+++ middlewares/validateTalkInfos.js
@@ -2,7 +2,7 @@ const validateTalkInfos = async (req, res, next) => {
   try {
     const { talk } = req.body;
 
-    if (!talk || !talk.rate || !talk.watchedAt) {
+    if (!talk || !talk.watchedAt || talk.rate === undefined) {
       return res.status(400).json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
       });
